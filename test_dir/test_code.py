20e81943c07e41ada7ac259038bca06444fb8e9d
Shreya Rao <95521864+shreyarao9@users.noreply.github.com>
2026-03-23 14:04:39 +0530

    Create test_code.py
    Add a new test_code.py file with print statements and a function.

---
+++ test_dir/test_code.py
@@ -0,0 +1,24 @@
+print('Hello')
+c = 4
+b = 6
+print(c+b)
+
+def abc(a,b,c,d,e,f,g):
+	print(a)
+	print(b)
+	print(c)
+	print(d)
+	print(e)
+	print(f)
+	print(g)
+	c=a+b
+	d=c+a
+	e=f+g
+	g=a+f
+	print(a)
+	print(b)
+	print(c)
+	c=a+b
+	d=c+a
+	e=f+g
+	g=a+f
